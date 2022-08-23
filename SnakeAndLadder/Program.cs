21c4d87f40a818f1f467b7bbbe12a3f72758d398
Prasad <banprasad21@gmail.com>
2022-08-23 23:59:41 +0530

    [Raghav] Add: Case to check for NoPlay / Ladder / Snake

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,12 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder");
-            int numberOfPlayers = 1;
-            int startPosition = 0;
-
+            int NUMBER_OF_PLAYERS = 1;
+            int START_POSITION = 0;
+            int presentPosition = 0;
+            const int NO_PLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
             Random random = new Random();
             int numberOnDie = random.Next(1, 7);
-            Console.WriteLine(numberOnDie);
+            //Console.WriteLine("You have got: " + numberOnDie);
+            int optionNumber = random.Next(0, 3);
+            switch (optionNumber)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    presentPosition += numberOnDie;
+                    break;
+                case SNAKE:
+                    presentPosition -= numberOnDie;
+                    break;
+
+            }
+            Console.WriteLine(presentPosition);
+
 
         }
     }
