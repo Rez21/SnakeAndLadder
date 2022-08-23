2fa6eb4f29913fbd8fca8e53f7730c458e370be5
Prasad <banprasad21@gmail.com>
2022-08-23 23:54:33 +0530

    [Raghav] Add: player roll the die get number from 1 to 6

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to snake and ladder");
+            Console.WriteLine("Welcome to Snake and Ladder");
             int numberOfPlayers = 1;
             int startPosition = 0;
+
+            Random random = new Random();
+            int numberOnDie = random.Next(1, 7);
+            Console.WriteLine(numberOnDie);
 
         }
     }
