7bb9cecefb0e827e73d0af1ac971644f332a5eae
Prasad <banprasad21@gmail.com>
2022-08-23 23:51:05 +0530

    [Raghav] Add: Single player at initial position 0

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ladder");
+            int numberOfPlayers = 1;
+            int startPosition = 0;
+
+        }
+    }
+}
